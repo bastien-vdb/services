a11108edfad7f12bca58b0b2c3933ae6e0525cdf
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-12-22 13:57:53 +0100

    feature/nextauth added

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
+
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+}
+
+export const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST};
