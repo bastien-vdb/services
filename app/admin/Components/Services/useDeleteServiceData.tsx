14e890de8c4501c2fbe2cc9613dab5ce186458cb
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-14 08:50:14 +0100

    fix/redeploy correction authOptions import usePersoHook3

--- app/admin/Components/Services/useDeleteServiceData.tsx
+++ app/admin/Components/Services/useDeleteServiceData.tsx
@@ -1,13 +1,13 @@
 'use server'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { prisma } from '@/src/db/prisma';
 import { Service } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import Stripe from 'stripe';
 
-async function useDeleteServerData({service }: {service: Service}) {
+async function useDeleteServerData({ service }: { service: Service }) {
 
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
   if (!process.env.STRIPE_SECRET_KEY) return new Response("Stripe secret key is not defined", { status: 400 });
   try {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2022-11-15" });
@@ -21,14 +21,15 @@ async function useDeleteServerData({service }: {service: Service}) {
     });
 
     const result = await prisma.service.findMany({
-        where: {
-            createdById: session?.user.id,
-        },
+      where: {
+        createdById: session?.user.id,
+      },
     });
     return result;
   } catch (error: unknown) {
     console.log("error", error);
-    throw new Error("Service cannot be deleted");}
+    throw new Error("Service cannot be deleted");
+  }
 }
 
 export default useDeleteServerData;
