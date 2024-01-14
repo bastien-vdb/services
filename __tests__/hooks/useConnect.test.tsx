85f362e1cf8c7f0ae3b806f67b3d321bd61c4f02
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-14 09:46:05 +0100

    fix/redeloy npm i all

--- __tests__/hooks/useConnect.test.tsx
+++ __tests__/hooks/useConnect.test.tsx
@@ -3,6 +3,7 @@ import { renderHook } from '@testing-library/react';
 describe('useConnect hook', () => {
     it('useConnect hook', () => {
         const { result } = renderHook(() => {
+            
             return useConnect();
         })
         expect(result.current).toBe(true);
