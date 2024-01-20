7ce14c70c285851b12127bbb769c84096ee3ffb6
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-20 09:40:52 +0100

    fix/paymentDone response fixed

--- app/api/checkout/paymentdone/route.ts
+++ app/api/checkout/paymentdone/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
 
-  return;
+      return res.json();
 }
 
 // export const config = {
