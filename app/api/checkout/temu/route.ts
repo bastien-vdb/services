3cc9e769b1b5cd28e1d8a7939b175116dacc610d
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-27 13:20:29 +0100

    fix/paymentDone response fixed4

--- app/api/checkout/temu/route.ts
+++ app/api/checkout/temu/route.ts
@@ -3,7 +3,7 @@ import { Stripe } from "stripe";
 import getRawBody from "raw-body";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response) {
-  console.log('la route temu fonctionne et est accessible');
-  return new NextResponse("la route temu fonctionne");
+export async function POST(req: Request, res: NextResponse) {
+    console.log('la route temu fonctionne et est accessible');
+    return new NextResponse("la route temu fonctionne");
 }
