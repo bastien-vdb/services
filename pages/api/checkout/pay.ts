b77b2f1a1cb17473cfab0381e43054fa95fbaab3
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-28 10:16:02 +0100

    fix/ pay4

--- pages/api/checkout/pay.ts
+++ pages/api/checkout/pay.ts
@@ -2,18 +2,21 @@ import { NextApiRequest, NextApiResponse } from "next";
 import getRawBody from "raw-body";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log('pay1 pass');
+    console.log('pay2 pass');
 
     try {
       if (req.method !== 'POST') {
           throw new Error('Method not allowed !!');
       }
 
-      const rawBody = await getRawBody(req);
+      // const rawBody = await getRawBody(req);
+      const rawBody = await req.body;
+
       console.log(rawBody);
       res.status(200).send("Webhook received");
   } catch (error) {
       console.error("Error processing request:", error);
       res.status(500).send("Internal Server Error");
   }
+  res.status(200).send("Webhook received");
 }
