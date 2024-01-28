b4ef06fda21f9d2be5adaaf9418c4ede2d3c5106
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-28 09:50:20 +0100

    fix/ pay2

--- pages/api/checkout/pay.ts
+++ pages/api/checkout/pay.ts
@@ -4,7 +4,7 @@ import getRawBody from "raw-body";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log('pay1 pass');
 
-  const rawBody = await getRawBody(req);
-  console.log(rawBody)
+  // const rawBody = await getRawBody(req);
+  // console.log(rawBody)
     res.status(200).send("Webhook received");
 }
