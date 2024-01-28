7900938294a495e5e531c935877ddf04e9e8111f
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-28 09:54:22 +0100

    fix/ pay3

--- pages/api/checkout/pay.ts
+++ pages/api/checkout/pay.ts
@@ -4,7 +4,16 @@ import getRawBody from "raw-body";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log('pay1 pass');
 
-  // const rawBody = await getRawBody(req);
-  // console.log(rawBody)
-    res.status(200).send("Webhook received");
+    try {
+      if (req.method !== 'POST') {
+          throw new Error('Method not allowed !!');
+      }
+
+      const rawBody = await getRawBody(req);
+      console.log(rawBody);
+      res.status(200).send("Webhook received");
+  } catch (error) {
+      console.error("Error processing request:", error);
+      res.status(500).send("Internal Server Error");
+  }
 }
