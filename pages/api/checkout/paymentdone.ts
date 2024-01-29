3e571f514b46b0a1374c1148b2a9d9943eed6af1
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-01-29 20:37:03 +0100

    fix/ pay15

--- pages/api/checkout/paymentdone.ts
+++ pages/api/checkout/paymentdone.ts
@@ -46,7 +46,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   console.log('après stockage signature', signature)
 
   // Construit l'événement à partir du corps de la requête et de la signature
-  const webhookEvent = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+  const webhookEvent = stripe.webhooks.constructEvent(buf, signature, process.env.STRIPE_WEBHOOK_SECRET);
   console.log('jusqu ici tout va bien');
 
       res.status(200).send("Webhook received");
