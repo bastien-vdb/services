da46afa80a441050cc0153a448a02b0506d09cd1
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-02-04 13:34:19 +0100

    feature/ajout email send + react email5

---
+++ src/emails/useSendEmail.ts
@@ -0,0 +1,19 @@
+"use server";
+import type { NextApiRequest, NextApiResponse } from "next";
+import EmailTemplate from './EmailTemplate';
+import { Resend } from "resend";
+
+const useSendEmail = async () => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["bastien.deboisrolin@gmail.com"],
+      subject: "Hello world",
+      react: EmailTemplate({ firstName: "John" }),
+    } as any) as any;
+    if (error) return error;
+    return data;
+};
+
+export default useSendEmail;
