b0a66dec4961f2bc0f8e3cfe6dc86b68b8db9b81
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-02-11 17:02:56 +0100

    fix/modification config img pour vercel

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        port: "3000",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "quickreserve",
         pathname: "/**",
       },
     ],
