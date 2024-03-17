7eee5b93b009146b7eb4586fa3542d299df956d9
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-03-17 19:46:51 +0100

    feature/ajout paypal3

--- app/Components/Steps.tsx
+++ app/Components/Steps.tsx
@@ -29,8 +29,8 @@ const Steps = ({ services, userId, bookings }: { services: Service[], userId: st
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                     </svg>
                 </Button>
-                <ServiceCalendar userId={userId} />
                 <SelectBooking bookings={bookings} userId={userId} />
+                <ServiceCalendar userId={userId} />
             </div>
         </main >
     );
