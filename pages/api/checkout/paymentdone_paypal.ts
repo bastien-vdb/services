6b4dba29d407fd13299d4b1a0f46796f146ef237
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-03-22 00:31:58 +0100

    fix/ajout event payment.capture.completed4

--- pages/api/checkout/paymentdone_paypal.ts
+++ pages/api/checkout/paymentdone_paypal.ts
@@ -1,9 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { buffer } from "micro"; // Utilisez 'micro' pour lire le corps brut de la requête
+import { buffer } from "micro";
+import useSendEmail from "@/src/emails/useSendEmail";
+import EmailRdvBooked from "@/src/emails/EmailBooked";
 
 export const config = {
   api: {
-    bodyParser: false, // Important pour pouvoir lire les streams
+    bodyParser: false,
   },
 };
 
@@ -11,56 +13,33 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Votre logique de traitement de webhook commence ici
-
-  // Lire le corps de la requête
   const buf = await buffer(req);
   const rawBody = buf.toString("utf-8");
 
-  // console.log("req ==>", req);
-
-  console.log("buf ==>", buf);
-
-  console.log("rawbody paypal ==>", rawBody);
-
-  // Extraire l'en-tête de transmission PayPal pour la validation
   const paypalTransmissionId = req.headers["paypal-transmission-id"];
 
-  console.log("paypal route ==>", paypalTransmissionId);
-
-  // Assurez-vous que toutes les variables nécessaires sont présentes
   if (!paypalTransmissionId) {
-    console.log("Requête invalide ==>", paypalTransmissionId);
     return res.status(400).send("Requête invalide");
   }
 
-  // Ici, vous effectueriez la validation de la signature du webhook avec l'API PayPal
-  // Cette étape est cruciale pour la sécurité et doit être implémentée selon la documentation officielle PayPal
-
   try {
-    // Parsez le corps de la requête pour obtenir l'objet de l'événement
     const webhookEvent = JSON.parse(rawBody);
-    console.log("webhookEvent ==>", webhookEvent);
 
-    // Exemple de gestion d'un événement de paiement réussi
     if (webhookEvent.event_type === "CHECKOUT.ORDER.APPROVED") {
-      // Logique spécifique à l'événement, par exemple, mise à jour de la base de données
-      console.log("Paiement réussi", webhookEvent);
-
-      // Réponse au serveur PayPal
       return res.status(200).send("Webhook traité avec succès");
     }
 
-    // Exemple de gestion d'un événement de paiement réussi
     if (webhookEvent.event_type === "PAYMENT.CAPTURE.COMPLETED") {
-      // Logique spécifique à l'événement, par exemple, mise à jour de la base de données
-      console.log("Paiement réussi", webhookEvent);
-
-      // Réponse au serveur PayPal
+      console.log("webhookEvent ==>", webhookEvent);
+      // const {email_address, } = webhookEvent.resource.payer;
+      // await useSendEmail({
+      //   from: "QuickReserve <no-answer@quickreserve.app>",
+      //   to: [String(payerEmail)],
+      //   subject: `${customerDetails.name} Votre créneau a bien été réservé`,
+      //   react: EmailRdvBooked({ customerName: customerDetails.name ?? "", bookingStartTime:bookingStartTime }),
+      // });
       return res.status(200).send("Webhook traité avec succès");
     }
-
-    // Ajoutez d'autres cas selon les types d'événements que vous gérez
   } catch (error) {
     console.error("Erreur lors du traitement du webhook:", error);
     return res.status(400).send("Erreur de traitement du webhook");
