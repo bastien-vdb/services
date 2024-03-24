1df4071404c096cf5382e2d124c96991e0783614
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-03-24 11:55:10 +0100

    fix/ajout event payment.capture.completed9

--- pages/api/checkout/paymentdone_paypal.ts
+++ pages/api/checkout/paymentdone_paypal.ts
@@ -30,7 +30,7 @@ export default async function handler(
     }
 
     if (webhookEvent.event_type === "PAYMENT.CAPTURE.COMPLETED") {
-      console.log("webhookEvent res ==>", webhookEvent.resource);
+      console.log("webhookEvent res ==>", webhookEvent);
       const { email_address } = webhookEvent.resource.payee;
       await useSendEmail({
         from: "QuickReserve <no-answer@quickreserve.app>",
