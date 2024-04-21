076e03836c7a6c763969db6c15f02d9ff1a4a9c5
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-04-21 14:45:12 +0200

    fix/deletion of system mode2

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           // defaultTheme="system"
-          // enableSystem
+          enableSystem={false}
           disableTransitionOnChange
         >
           <AuthSessionProvider>{children}</AuthSessionProvider>
