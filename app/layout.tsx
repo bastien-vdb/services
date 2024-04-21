b0ea1394bb4c4a40ebce53f9ae9349387baafad9
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-04-21 14:39:05 +0200

    fix/deletion of system mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,56 @@
-import '@/app/globals.css';
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/src/contexts/theme-provider';
-import AuthSessionProvider from '@/src/components/Auth/AuthSessionProvider';
-import Header from '@/src/components/Header/Header';
-import { Toaster } from "@/src/components/ui/toaster"
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/src/contexts/theme-provider";
+import AuthSessionProvider from "@/src/components/Auth/AuthSessionProvider";
+import Header from "@/src/components/Header/Header";
+import { Toaster } from "@/src/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Service manager',
-  description: 'Service manager app with React',
-}
+  title: "Service manager",
+  description: "Service manager app with React",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className={inter.className}>
         <Toaster />
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          // defaultTheme="system"
+          // enableSystem
           disableTransitionOnChange
         >
-          <AuthSessionProvider>
-            {children}
-          </AuthSessionProvider>
+          <AuthSessionProvider>{children}</AuthSessionProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
