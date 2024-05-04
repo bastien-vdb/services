2a7e883be7dd76d488671c8e72a8a9b652ece627
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-05-04 23:58:17 +0200

    feature/changement paymentDone3

--- app/admin/Components/Bookings/BookingsPayed.tsx
+++ app/admin/Components/Bookings/BookingsPayed.tsx
@@ -10,58 +10,60 @@ import { useMemo, useState } from "react";
 import useBookingStore from "../Calendar/useAvailabilityStore";
 
 function BookingsPayed({ services }: { services: Service[] }) {
-  const { bookings, deleteBooking } = useBookingStore();
+  // const { bookings, deleteBooking } = useBookingStore();
 
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
-  const handleDeleteBooking = async (bookingId: string) => {
-    if (!bookingId) return;
-    setLoading(true);
-    await deleteBooking(bookingId);
-    setLoading(false);
-    toast({
-      variant: "success",
-      description: "Réservation supprimée",
-    });
-  };
+  // const handleDeleteBooking = async (bookingId: string) => {
+  //   if (!bookingId) return;
+  //   setLoading(true);
+  //   await deleteBooking(bookingId);
+  //   setLoading(false);
+  //   toast({
+  //     variant: "success",
+  //     description: "Réservation supprimée",
+  //   });
+  // };
 
-  //Pour optimiser la recherche de service on crée un map. (et en plus on le useMemo)
-  const serviceMap: Record<string, Service> = services.reduce(
-    (serviceMap, service) => {
-      serviceMap[service.id] = service;
-      return serviceMap;
-    },
-    {}
-  );
+  // //Pour optimiser la recherche de service on crée un map. (et en plus on le useMemo)
+  // const serviceMap: Record<string, Service> = services.reduce(
+  //   (serviceMap, service) => {
+  //     serviceMap[service.id] = service;
+  //     return serviceMap;
+  //   },
+  //   {}
+  // );
 
-  const data = useMemo(
-    () =>
-      bookings
-        .filter((booking) => booking.status === "CONFIRMED")
-        .map((booking) => {
-          return {
-            id: booking.id,
-            du: moment(booking.startTime)
-              .format("DD/MM/YYYY - HH:mm")
-              .toString(),
-            au: moment(booking.endTime).format("DD/MM/YYYY - HH:mm").toString(),
-            userEmail: String(booking.payedBy) ?? "Non renseigné",
-            prestation: serviceMap[booking.serviceId]?.name,
-            prix: serviceMap[booking.serviceId]?.price,
-            annuler: (
-              <AlertModal
-                disabled={loading}
-                onAction={() => handleDeleteBooking(booking.id)}
-              >
-                <Trash2 className="text-destructive"></Trash2>
-              </AlertModal>
-            ),
-          };
-        }),
-    [bookings]
-  );
+  // const data = useMemo(
+  //   () =>
+  //     bookings
+  //       .filter((booking) => booking.status === "CONFIRMED")
+  //       .map((booking) => {
+  //         return {
+  //           id: booking.id,
+  //           du: moment(booking.startTime)
+  //             .format("DD/MM/YYYY - HH:mm")
+  //             .toString(),
+  //           au: moment(booking.endTime).format("DD/MM/YYYY - HH:mm").toString(),
+  //           userEmail: String(booking.payedBy) ?? "Non renseigné",
+  //           prestation: serviceMap[booking.serviceId]?.name,
+  //           prix: serviceMap[booking.serviceId]?.price,
+  //           annuler: (
+  //             <AlertModal
+  //               disabled={loading}
+  //               onAction={() => handleDeleteBooking(booking.id)}
+  //             >
+  //               <Trash2 className="text-destructive"></Trash2>
+  //             </AlertModal>
+  //           ),
+  //         };
+  //       }),
+  //   [bookings]
+  // );
 
-  return <DataTable columns={columns} data={data} />;
+  // return <DataTable columns={columns} data={data} />;
+
+  return <></>;
 }
 
 export default BookingsPayed;
