e20d7d9bfb80fbe27daa35c91457a1ca7c5d8b78
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-05-05 00:00:31 +0200

    feature/changement paymentDone4

--- app/admin/Components/Bookings/Bookings.tsx
+++ app/admin/Components/Bookings/Bookings.tsx
@@ -7,46 +7,48 @@ import moment from "moment";
 import useBookingStore from "../Calendar/useAvailabilityStore";
 
 function Bookings() {
-  const { bookings, changeStatus } = useBookingStore();
+  // const { bookings, changeStatus } = useBookingStore();
 
-  const handleActiveBooking = (bookingId: string) => {
-    changeStatus(bookingId, "AVAILABLE");
-    toast({
-      variant: "success",
-      title: "Rendez-vous activé",
-      description: "Le rendez-vous est maintenant disponible",
-    });
-  };
+  // const handleActiveBooking = (bookingId: string) => {
+  //   changeStatus(bookingId, "AVAILABLE");
+  //   toast({
+  //     variant: "success",
+  //     title: "Rendez-vous activé",
+  //     description: "Le rendez-vous est maintenant disponible",
+  //   });
+  // };
 
-  const handleCancelBooking = (bookingId: string) => {
-    changeStatus(bookingId, "CANCELLED");
-    toast({
-      variant: "success",
-      title: "Rendez-vous désactivé",
-      description: "Le rendez-vous n'est plus disponible",
-    });
-  };
+  // const handleCancelBooking = (bookingId: string) => {
+  //   changeStatus(bookingId, "CANCELLED");
+  //   toast({
+  //     variant: "success",
+  //     title: "Rendez-vous désactivé",
+  //     description: "Le rendez-vous n'est plus disponible",
+  //   });
+  // };
 
-  const formatDataToServiceTableBody = bookings.map((b) => {
-    return {
-      id: b.id,
-      du: moment(b.startTime).format("DD/MM/YYYY - HH:mm:ss").toString(),
-      au: moment(b.endTime).format("DD/MM/YYYY - HH:mm:ss").toString(),
-      actif: (
-        <Switch
-          className="float-right"
-          checked={b.status !== "CANCELLED"}
-          onCheckedChange={() =>
-            b.status !== "CANCELLED"
-              ? handleCancelBooking(b.id)
-              : handleActiveBooking(b.id)
-          }
-        />
-      ),
-    };
-  });
+  // const formatDataToServiceTableBody = bookings.map((b) => {
+  //   return {
+  //     id: b.id,
+  //     du: moment(b.startTime).format("DD/MM/YYYY - HH:mm:ss").toString(),
+  //     au: moment(b.endTime).format("DD/MM/YYYY - HH:mm:ss").toString(),
+  //     actif: (
+  //       <Switch
+  //         className="float-right"
+  //         checked={b.status !== "CANCELLED"}
+  //         onCheckedChange={() =>
+  //           b.status !== "CANCELLED"
+  //             ? handleCancelBooking(b.id)
+  //             : handleActiveBooking(b.id)
+  //         }
+  //       />
+  //     ),
+  //   };
+  // });
 
-  return <DataTable columns={columns} data={formatDataToServiceTableBody} />;
+  // return <DataTable columns={columns} data={formatDataToServiceTableBody} />;
+
+  return <></>;
 }
 
 export default Bookings;
