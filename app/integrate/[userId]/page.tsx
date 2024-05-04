ef9e5b3f3e985a69441ec6902127fb5266495436
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-05-05 00:02:21 +0200

    feature/changement paymentDone5

--- app/integrate/[userId]/page.tsx
+++ app/integrate/[userId]/page.tsx
@@ -1,40 +1,36 @@
 import { prisma } from "@/src/db/prisma";
-import Steps from '@/app/Components/Steps';
-import { Booking, Service } from '@prisma/client';
-
+import Steps from "@/app/Components/Steps";
+import { Booking, Service } from "@prisma/client";
 
 async function Business({ params }: { params: { userId: string } }) {
+  const { userId } = params;
 
-    const { userId } = params;
+  let services: Service[] = [];
+  let bookings: Booking[] = [];
 
-    let services: Service[] = [];
-    let bookings: Booking[] = [];
+  try {
+    const res = await prisma.service.findMany({
+      where: {
+        createdById: userId,
+      },
+    });
+    services.push(...res);
+  } catch (error) {
+    throw new Error("Data cannot be reach from the db");
+  }
 
-    try {
-        const res = await prisma.service.findMany({
-            where: {
-                createdById: userId,
-            },
-        })
-        services.push(...res);
+  try {
+    const res = await prisma.booking.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+    bookings.push(...res);
+  } catch (error) {
+    throw new Error("Data cannot be reach from the db");
+  }
 
-    } catch (error) {
-        throw new Error("Data cannot be reach from the db");
-    }
-
-    try {
-        const res = await prisma.booking.findMany({
-            where: {
-                userId: userId,
-            },
-        })
-        bookings.push(...res);
-
-    } catch (error) {
-        throw new Error("Data cannot be reach from the db");
-    }
-
-    return <Steps services={services} userId={userId} bookings={bookings} />
+  return <Steps services={services} userId={userId} />;
 }
 
 export default Business;
