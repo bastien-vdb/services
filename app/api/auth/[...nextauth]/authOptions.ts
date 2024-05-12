1b8124739ad81ab7e097a18d5d66bf01e5115ce5
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-05-12 03:55:54 +0200

    feature/ajout login par magic link

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,9 +1,38 @@
+import { prisma } from "@/src/db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
+import { SendVerificationRequestParams } from "next-auth/providers";
+import EmailProvider from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/src/db/prisma";
+import { Resend } from "resend";
+
+// resend.ts
+export const sendVerificationRequest = async (
+  params: SendVerificationRequestParams
+) => {
+  let {
+    identifier: email,
+    url,
+    provider: { from },
+  } = params;
+  try {
+    const resend = new Resend(process.env.RESEND_API_KEY!);
+    await resend.emails.send({
+      from: from,
+      to: email,
+      subject: "Login Link to your Account",
+      html:
+        '<p>Click the magic link below to sign in to your account:</p>\
+             <p><a href="' +
+        url +
+        '"><b>Sign in</b></a></p>',
+    });
+  } catch (error) {
+    console.log({ error });
+  }
+};
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -15,8 +44,13 @@ export const authOptions: NextAuthOptions = {
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string
-    })
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    EmailProvider({
+      from: "noreply@quickreserve.app",
+      // Custom sendVerificationRequest() function
+      sendVerificationRequest,
+    }),
     // ...add more providers here
   ],
   callbacks: {
