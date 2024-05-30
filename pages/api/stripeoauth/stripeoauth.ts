3bc4f99a05a71bfa770039e5d5652058677a81ea
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-05-30 20:27:54 +0200

    fix/changement route stripeoauth6

--- pages/api/stripeoauth/stripeoauth.ts
+++ pages/api/stripeoauth/stripeoauth.ts
@@ -1,5 +1,4 @@
 import useCheckStripe from "@/src/hooks/useCheckStripe";
-import { useStripe } from "@stripe/react-stripe-js";
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/src/db/prisma";
 
@@ -8,11 +7,20 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    const { code, state } = req.body;
+    const { code, state } = req.query as { code: string; state: string }; // Utilisation de req.query pour GET
+
+    if (!code || !state) {
+      return res
+        .status(400)
+        .json({ error: "Code and state parameters are required" });
+    }
 
     const stripe = useCheckStripe();
 
     try {
+      console.log("Code:", code); // Log le code pour déboguer
+      console.log("State:", state); // Log l'état pour déboguer
+
       const response = await stripe.oauth.token({
         grant_type: "authorization_code",
         code,
@@ -27,11 +35,18 @@ export default async function handler(
 
       res.status(200).json({ connected_account_id });
     } catch (err) {
-      console.error("Stripe OAuth error:", err);
+      console.error("Stripe OAuth error:", err); // Log l'erreur complète pour plus de détails
+      if (err.raw) {
+        console.error("Stripe Error Details:", {
+          statusCode: err.raw.statusCode,
+          requestId: err.raw.requestId,
+          message: err.raw.message,
+        });
+      }
       res.status(500).json({ error: "Failed to connect Stripe account" });
     }
   } else {
-    res.setHeader("Allow", ["POST"]);
+    res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
