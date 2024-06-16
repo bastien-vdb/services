7081c3f143f79368c7c449678ba952a9f17e2e55
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-06-16 16:08:20 +0200

    wip changement version api dans hook stripe dans mon code

--- pages/api/checkout/paymentdone.ts
+++ pages/api/checkout/paymentdone.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import getRawBody from "raw-body";
+import type { Readable } from "node:stream";
 import useSendEmail from "@/src/emails/useSendEmail";
 import EmailRdvBooked from "@/src/emails/EmailBooked";
 import useCheckStripe from "@/src/hooks/useCheckStripe";
@@ -25,185 +25,170 @@ type metadataType = {
   formData: string;
 };
 
+async function buffer(readable: Readable) {
+  const chunks: any = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const stripe = useCheckStripe();
 
-  let rawBody;
-  try {
-    const encoding = req.headers["content-type"]?.includes("charset=utf-8")
-      ? "utf-8"
-      : undefined;
-    rawBody = await getRawBody(req, {
-      length: req.headers["content-length"]
-        ? parseInt(req.headers["content-length"], 10)
-        : undefined,
-      encoding: encoding,
-    });
-  } catch (err) {
-    console.error("Error reading raw body:", err);
-    return res.status(500).send(`Error reading raw body: ${err.message}`);
-  }
-
-  const rawBodyString = rawBody.toString("utf8");
-  console.log("rawBody", rawBodyString);
+  const buf = await buffer(req);
 
   if (!process.env.STRIPE_WEBHOOK_SECRET)
     throw new Error("Stripe webhook secret key is not defined");
 
   const signature = req.headers["stripe-signature"];
-  console.log("signature", signature);
 
-  if (!signature) throw new Error("Stripe signature is not defined");
+  if (signature === undefined)
+    throw new Error("Stripe signature is not defined");
 
-  let webhookEvent;
-
-  try {
-    webhookEvent = stripe.webhooks.constructEvent(
-      rawBody,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err) {
-    console.error("Error verifying Stripe signature:", err.message);
-    return res.status(400).send(`Webhook error: ${err.message}`);
-  }
-
-  console.log("webhookEvent", webhookEvent);
+  const webhookEvent = stripe.webhooks.constructEvent(
+    buf,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET
+  );
 
   try {
-    switch (webhookEvent.type) {
-      case "payment_intent.succeeded": {
-        // Handle the event
-        const session = webhookEvent.data.object.metadata as metadataType;
-        const customerDetails = webhookEvent.data.object.customer_details;
-        const {
-          startTime,
-          endTime,
-          serviceId,
-          userId,
-          serviceName,
-          addedOption,
-          formData,
-        } = session;
+    switch (
+      webhookEvent.type
+      //   case "payment_intent.succeeded": {
+      // switch (webhookEvent.type) {
+      //   case "payment_intent.succeeded": {
+      //     const session = webhookEvent.data.object.metadata as metadataType;
+      //     const customerDetails = webhookEvent.data.object.customer_details;
+      //     const {
+      //       startTime,
+      //       endTime,
+      //       serviceId,
+      //       userId,
+      //       serviceName,
+      //       addedOption,
+      //       formData,
+      //     } = session;
 
-        const startDateTmz = moment
-          .utc(startTime)
-          .tz("Europe/Paris")
-          .format("YYYY-MM-DD HH:mm:ss");
+      //     const startDateTmz = moment
+      //       .utc(startTime)
+      //       .tz("Europe/Paris")
+      //       .format("YYYY-MM-DD HH:mm:ss");
 
-        const bookingCreated = await actionCreateBooking({
-          startTime: new Date(startTime),
-          endTime: new Date(endTime),
-          serviceId,
-          userId,
-          amountPayed: webhookEvent.data.object.amount_total,
-          form: formData,
-          customerInfo: {
-            name: customerDetails?.name ? customerDetails.name : "NC",
-            email: customerDetails?.email ? customerDetails.email : "NC",
-            phone: customerDetails?.phone ? customerDetails.phone : "NC",
-            address: {
-              city: customerDetails?.address?.city
-                ? customerDetails.address.city
-                : "NC",
-              country: customerDetails?.address?.country
-                ? customerDetails.address.country
-                : "NC",
-              state: customerDetails?.address?.state
-                ? customerDetails.address.state
-                : "NC",
-              zip: customerDetails?.address?.postal_code
-                ? customerDetails.address.postal_code
-                : "NC",
-              line1: customerDetails?.address?.line1
-                ? customerDetails.address.line1
-                : "NC",
-              line2: customerDetails?.address?.line2
-                ? customerDetails.address.line2
-                : "NC",
-            },
-          },
-        });
+      //     const bookingCreated = await actionCreateBooking({
+      //       startTime: new Date(startTime),
+      //       endTime: new Date(endTime),
+      //       serviceId,
+      //       userId,
+      //       amountPayed: webhookEvent.data.object.amount_total,
+      //       form: formData,
+      //       customerInfo: {
+      //         name: customerDetails?.name ? customerDetails.name : "NC",
+      //         email: customerDetails?.email ? customerDetails.email : "NC",
+      //         phone: customerDetails?.phone ? customerDetails.phone : "NC",
+      //         address: {
+      //           city: customerDetails?.address?.city
+      //             ? customerDetails.address.city
+      //             : "NC",
+      //           country: customerDetails?.address?.country
+      //             ? customerDetails.address.country
+      //             : "NC",
+      //           state: customerDetails?.address?.state
+      //             ? customerDetails.address.state
+      //             : "NC",
+      //           zip: customerDetails?.address?.postal_code
+      //             ? customerDetails.address.postal_code
+      //             : "NC",
+      //           line1: customerDetails?.address?.line1
+      //             ? customerDetails.address.line1
+      //             : "NC",
+      //           line2: customerDetails?.address?.line2
+      //             ? customerDetails.address.line2
+      //             : "NC",
+      //         },
+      //       },
+      //     });
 
-        if (bookingCreated && customerDetails?.email) {
-          await useSendEmail({
-            from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
-            to: [customerDetails.email],
-            subject: `Rendez-vous ${serviceName} en attente.`,
-            react: EmailRdvBooked({
-              customerName: customerDetails.name ?? "",
-              bookingStartTime: startDateTmz,
-              serviceName,
-              employeeName: "Natacha S",
-              businessPhysicalAddress: "36 chemin des huats, 93000 Bobigny",
-            }),
-          });
-        }
-        if (!bookingCreated && customerDetails?.email) {
-          await useSendEmail({
-            from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
-            to: [String(customerDetails.email)],
-            subject: `${customerDetails.name} Votre n'a pas pu être réservé`,
-            react: EmailNotBooked({
-              customerName: customerDetails.name ?? "",
-              bookingStartTime: startDateTmz,
-            }),
-          });
-        }
-        break;
-      }
-      case "checkout.session.expired": {
-        const session = webhookEvent.data.object.metadata as metadataType;
-        const customerDetails = webhookEvent.data.object.customer_details;
-        const { startTime } = session;
+      //     if (bookingCreated && customerDetails?.email) {
+      //       await useSendEmail({
+      //         from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
+      //         to: [customerDetails.email],
+      //         subject: `Rendez-vous ${serviceName} en attente.`,
+      //         react: EmailRdvBooked({
+      //           customerName: customerDetails.name ?? "",
+      //           bookingStartTime: startDateTmz,
+      //           serviceName,
+      //           employeeName: "Natacha S",
+      //           businessPhysicalAddress: "36 chemin des huats, 93000 Bobigny",
+      //         }),
+      //       });
+      //     }
+      //     if (!bookingCreated && customerDetails?.email) {
+      //       await useSendEmail({
+      //         from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
+      //         to: [String(customerDetails.email)],
+      //         subject: `${customerDetails.name} Votre n'a pas pu être réservé`,
+      //         react: EmailNotBooked({
+      //           customerName: customerDetails.name ?? "",
+      //           bookingStartTime: startDateTmz,
+      //         }),
+      //       });
+      //     }
+      //     break;
+      //   }
 
-        const startDateTmz = moment
-          .utc(startTime)
-          .tz("Europe/Paris")
-          .format("YYYY-MM-DD HH:mm:ss");
+      //   case "checkout.session.expired": {
+      //     const session = webhookEvent.data.object.metadata as metadataType;
+      //     const customerDetails = webhookEvent.data.object.customer_details;
+      //     const { startTime } = session;
 
-        if (customerDetails) {
-          await useSendEmail({
-            from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
-            to: [String(customerDetails.email)],
-            subject: `${customerDetails.name} Votre n'a pas pu être réservé`,
-            react: EmailNotBooked({
-              customerName: customerDetails.name ?? "",
-              bookingStartTime: startDateTmz,
-            }),
-          });
-        }
-        break;
-      }
+      //     const startDateTmz = moment
+      //       .utc(startTime)
+      //       .tz("Europe/Paris")
+      //       .format("YYYY-MM-DD HH:mm:ss");
 
-      case "checkout.session.async_payment_failed": {
-        const session = webhookEvent.data.object.metadata as metadataType;
-        const customerDetails = webhookEvent.data.object.customer_details;
-        const { startTime } = session;
+      //     if (customerDetails) {
+      //       await useSendEmail({
+      //         from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
+      //         to: [String(customerDetails.email)],
+      //         subject: `${customerDetails.name} Votre n'a pas pu être réservé`,
+      //         react: EmailNotBooked({
+      //           customerName: customerDetails.name ?? "",
+      //           bookingStartTime: startDateTmz,
+      //         }),
+      //       });
+      //     }
+      //     break;
+      //   }
 
-        const startDateTmz = moment
-          .utc(startTime)
-          .tz("Europe/Paris")
-          .format("YYYY-MM-DD HH:mm:ss");
+      //   case "checkout.session.async_payment_failed": {
+      //     const session = webhookEvent.data.object.metadata as metadataType;
+      //     const customerDetails = webhookEvent.data.object.customer_details;
+      //     const { startTime } = session;
 
-        if (customerDetails) {
-          await useSendEmail({
-            from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
-            to: [String(customerDetails.email)],
-            subject: `${customerDetails.name} Votre n'a pas pu être réservé`,
-            react: EmailNotBooked({
-              customerName: customerDetails.name ?? "",
-              bookingStartTime: startDateTmz,
-            }),
-          });
-        }
-        break;
-      }
-      default:
-        console.log(`Unhandled event type ${webhookEvent.type}`);
+      //     const startDateTmz = moment
+      //       .utc(startTime)
+      //       .tz("Europe/Paris")
+      //       .format("YYYY-MM-DD HH:mm:ss");
+
+      //     if (customerDetails) {
+      //       await useSendEmail({
+      //         from: "Finest lash - Quickreserve.app <no-answer@quickreserve.app>",
+      //         to: [String(customerDetails.email)],
+      //         subject: `${customerDetails.name} Votre n'a pas pu être réservé`,
+      //         react: EmailNotBooked({
+      //           customerName: customerDetails.name ?? "",
+      //           bookingStartTime: startDateTmz,
+      //         }),
+      //       });
+      //     }
+      // break;
+      // }
+      // default:
+    ) {
     }
     res.status(200).send("Webhook received");
   } catch (error) {
--- src/hooks/useCheckStripe.ts
+++ src/hooks/useCheckStripe.ts
@@ -2,7 +2,10 @@ import Stripe from "stripe";
 
 const useCheckStripe = () => {
   if (!process.env.STRIPE_SECRET_KEY) throw new Error("stripe key missing");
-  return new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2023-10-16" });
+  return new Stripe(process.env.STRIPE_SECRET_KEY, {
+    // @ts-ignore: Ignore API version type error
+    apiVersion: "2024-04-10",
+  });
 };
 
 export default useCheckStripe;
