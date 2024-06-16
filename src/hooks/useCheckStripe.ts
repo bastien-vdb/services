5b45ef1f100823233da8965ddb61ebc57b92fb6a
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-06-16 16:17:41 +0200

    upgrade stripe v package.json

--- src/hooks/useCheckStripe.ts
+++ src/hooks/useCheckStripe.ts
@@ -3,7 +3,6 @@ import Stripe from "stripe";
 const useCheckStripe = () => {
   if (!process.env.STRIPE_SECRET_KEY) throw new Error("stripe key missing");
   return new Stripe(process.env.STRIPE_SECRET_KEY, {
-    // @ts-ignore: Ignore API version type error
     apiVersion: "2024-04-10",
   });
 };
