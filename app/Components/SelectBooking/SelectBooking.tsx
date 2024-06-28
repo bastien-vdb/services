54eb200b7781dc44bcbf528f37c3e6a20a3e2bf7
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-06-29 00:53:55 +0200

    recherche bug date

--- app/Components/SelectBooking/SelectBooking.tsx
+++ app/Components/SelectBooking/SelectBooking.tsx
@@ -88,8 +88,13 @@ const SelectBooking = ({
                           scrollNext();
                         }}
                       >
-                        {moment(booking.startTime).format("HH:mm").toString()}-
-                        {moment(booking.endTime).format("HH:mm").toString()}
+                        {moment(booking.startTime)
+                          .format("DD/MM HH:mm")
+                          .toString()}
+                        -
+                        {moment(booking.endTime)
+                          .format("DD/MM HH:mm")
+                          .toString()}
                       </Button>
                     </li>
                   ))
--- app/admin/Components/Bookings/action-createBooking.tsx
+++ app/admin/Components/Bookings/action-createBooking.tsx
@@ -38,8 +38,6 @@ async function actionCreateBooking({
       throw new Error("No availability found");
     }
 
-    console.log("availability av supr ==>", availability);
-
     // Gérer la création de la réservation et la mise à jour des disponibilités dans une transaction
     return await prisma.$transaction(async (prisma) => {
       // Si la réservation commence après le début de la disponibilité
@@ -64,14 +62,10 @@ async function actionCreateBooking({
         });
       }
 
-      console.log("availability av supr ==>", availability);
-
       // Supprimer l'ancienne disponibilité
       await prisma.availability.delete({
         where: { id: availability.id },
       });
-
-      console.log("availability ap supr ==>", availability);
 
       // Rechercher ou créer un client basé sur l'email fourni
       let customer = await prisma.customer.findFirst({
