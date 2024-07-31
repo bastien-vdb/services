cf12f495f0087ca2795f06452a90adf29c25ae0f
VERMOT Bastien <bastien.vermotdeboisrolin@orange.com>
2024-07-31 19:29:53 +0200

    ajout cron job deletion availabilities

--- app/api/cron-delete-availability/route.ts
+++ app/api/cron-delete-availability/route.ts
@@ -11,6 +11,7 @@ export async function DELETE(req: Request, res: NextApiResponse) {
       },
     });
 
+    console.log(`Job: Deleted: ${count} old availabilities`);
     return res.status(200).json({
       message: "Old availabilities deleted successfully",
       count,
